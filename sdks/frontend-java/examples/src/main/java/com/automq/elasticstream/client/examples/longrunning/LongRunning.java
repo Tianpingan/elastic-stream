c15d56797fc3d5b21a538c7478781c44078aaf84
tianpingan <tianpingan2000@gmail.com>
2023-08-08 06:09:29 +0000

    fix(longrunning): Fix typo
    Signed-off-by: tianpingan <tianpingan2000@gmail.com>

--- sdks/frontend-java/examples/src/main/java/com/automq/elasticstream/client/examples/longrunning/LongRunning.java
+++ sdks/frontend-java/examples/src/main/java/com/automq/elasticstream/client/examples/longrunning/LongRunning.java
@@ -10,38 +10,38 @@ import com.automq.elasticstream.client.api.CreateStreamOptions;
 import com.automq.elasticstream.client.api.Stream;
 
 public class LongRunning {
-        private static Logger log = Logger.getLogger(LongRunning.class.getClass());
+    private static Logger log = Logger.getLogger(LongRunning.class.getClass());
 
-        public static void main(String[] args) throws Exception {
-                LongRunningOption option = new LongRunningOption();
-                log.info("EndPoint: " + option.getEndPoint() + ", KvEndPoint: " +
-                                option.getKvEndPoint()
-                                + ", AppendInterval: " +
-                                option.getInterval()
-                                + ", PayloadSizeMin: "
-                                + option.getMin() + ", PayloadSizeMax: " + option.getMax());
-                Client client = Client.builder().endpoint(option.getEndPoint()).kvEndpoint(option.getKvEndPoint())
-                                .build();
-                ExecutorService executor = Executors.newFixedThreadPool(9);
-                for (int replica = 1; replica <= 3; replica++) {
-                        Stream stream = client.streamClient()
-                                        .createAndOpenStream(
-                                                        CreateStreamOptions.newBuilder().replicaCount(replica).build())
-                                        .get();
-                        createTask(stream, Utils.getRandomInt(0, 1024), option.getMin(),
-                                        option.getMax(), option.getInterval(), executor);
-                }
+    public static void main(String[] args) throws Exception {
+        LongRunningOption option = new LongRunningOption();
+        log.info("EndPoint: " + option.getEndPoint() + ", KvEndPoint: " +
+                option.getKvEndPoint()
+                + ", AppendInterval: " +
+                option.getInterval()
+                + ", PayloadSizeMin: "
+                + option.getMin() + ", PayloadSizeMax: " + option.getMax());
+        Client client = Client.builder().endpoint(option.getEndPoint()).kvEndpoint(option.getKvEndPoint())
+                .build();
+        ExecutorService executor = Executors.newFixedThreadPool(9);
+        for (int replica = 1; replica <= 3; replica++) {
+            Stream stream = client.streamClient()
+                    .createAndOpenStream(
+                            CreateStreamOptions.newBuilder().replicaCount(replica).build())
+                    .get();
+            createTask(stream, Utils.getRandomInt(0, 1024), option.getMin(),
+                    option.getMax(), option.getInterval(), executor);
         }
+    }
 
-        private static void createTask(Stream stream,
-                        int startSeq,
-                        int minSize,
-                        int maxSize,
-                        long interval, ExecutorService executor) {
-                AtomicLong endOffset = new AtomicLong(0);
-                executor.submit(new Producer(stream, startSeq, minSize, maxSize, interval, endOffset));
-                executor.submit(new TailReadConsumer(stream,
-                                startSeq, endOffset));
-                executor.submit(new RepeatedReadConsumer(stream, startSeq, endOffset));
-        }
+    private static void createTask(Stream stream,
+            int startSeq,
+            int minSize,
+            int maxSize,
+            long interval, ExecutorService executor) {
+        AtomicLong endOffset = new AtomicLong(0);
+        executor.submit(new Producer(stream, startSeq, minSize, maxSize, interval, endOffset));
+        executor.submit(new TailReadConsumer(stream,
+                startSeq, endOffset));
+        executor.submit(new RepeatedReadConsumer(stream, startSeq, endOffset));
+    }
 }
