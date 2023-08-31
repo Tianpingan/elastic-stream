6a42919852fb6c1f16ecd9d1b5df093824b59faa
tianpingan <tianpingan2000@gmail.com>
2023-08-31 06:45:22 +0000

    Fix comments
    Signed-off-by: tianpingan <tianpingan2000@gmail.com>

--- sdks/frontend-java/tools/src/main/java/com/automq/elasticstream/client/tools/e2e/AppendTest.java
+++ sdks/frontend-java/tools/src/main/java/com/automq/elasticstream/client/tools/e2e/AppendTest.java
@@ -13,7 +13,7 @@ public class AppendTest {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         E2EOption option = new E2EOption();
         Client client = Client.builder().endpoint(option.getEndPoint()).kvEndpoint(option.getKvEndPoint()).build();
-        // 1. 发送数据到全新 Stream
+
         Stream stream0 = client.streamClient()
                 .createAndOpenStream(CreateStreamOptions.newBuilder().epoch(0)
                         .replicaCount(option.getReplica()).build())
@@ -22,13 +22,13 @@ public class AppendTest {
         assertTrue(Utils.appendRecords(stream0, 0, option.getCount(), option.getBatchSize()));
         assertTrue(Utils.fetchRecords(stream0, 0, option.getCount(), option.getBatchSize()));
         stream0.close().get();
-        // 2. 发送数据到一个重新 Open 的 Stream
+
         Stream stream1 = Utils.openStream(client, streamId, OpenStreamOptions.newBuilder().epoch(1).build());
         assertTrue(stream1 != null);
         assertTrue(Utils.appendRecords(stream1, option.getCount(), option.getCount(), option.getBatchSize()));
         assertTrue(Utils.fetchRecords(stream1, option.getCount(), option.getCount(), option.getBatchSize()));
         stream1.close().get();
-        // 3. 以旧的epoch去打开，期望是open失败
+
         Stream stream2 = Utils.openStream(client, streamId, OpenStreamOptions.newBuilder().epoch(0).build());
         assertTrue(stream2 == null);
         System.out.println("PASS");
